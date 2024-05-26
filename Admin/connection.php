5d5b358d4141cbd48aa6dcfd4a24a8443b0ca0c3
Mark <markmarkmagdy1010@gmail.com>
2024-05-26 22:33:23 +0300

    connection file

---
+++ Admin/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+// $host = "";
+// $user = "";
+// $pass = "";
+// $db = "";
+
+$con = mysqli_connect("localhost", "root", "", "traveling");
+if ($con) {
+
+} else {
+    die("Error Connection Failed");
+}
+// $con->set_charset("utf8");
+
+?>
